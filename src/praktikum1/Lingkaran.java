60a9812c8633a20238c01923fdbfa378dc08492f
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-02-07 21:58:03 +0700

    Praktikum1

---
+++ src/praktikum1/Lingkaran.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+/**
+ *
+ * @author rizwa
+ */
+public class Lingkaran {
+    // method penghitungan
+    public void menghitungluas(double r){
+        double luaslingkaran = (double)(Math.PI*r*r);
+        System.out.println("Luas Permukaan Bola adalah: "+luaslingkaran+" cm²");
+    }
+}
+
---
+++ src/praktikum1/Persegi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum1;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Persegi {
+    public void menghitungluas(double s){
+        double luaspersegi = (s*s);
+        System.out.println("Luas Persegi adalah: "+luaspersegi+" cm²");
+    }
+}
+
