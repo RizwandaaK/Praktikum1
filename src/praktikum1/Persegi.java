a3078a3db62201f59de158c91601cb90d7d7907a
RizwandaaK <73205628+RizwandaaK@users.noreply.github.com>
2021-02-15 13:24:38 +0700

    Update Persegi.java

--- src/praktikum1/Persegi.java
+++ src/praktikum1/Persegi.java
@@ -4,15 +4,29 @@
  * and open the template in the editor.
  */
 package praktikum1;
+import java.util.Scanner;
 
 /**
  *
  * @author rizwa
  */
 public class Persegi {
-    public void menghitungluas(double s){
-        double luaspersegi = (s*s);
-        System.out.println("Luas Persegi adalah: "+luaspersegi+" cm²");
+    Scanner input = new Scanner(System.in);
+    
+    String bangundatar;
+    double p,l,hasil;
+    
+    void luaspersegi() {
+        System.out.println("");
+        System.out.println("Menghitung Luas Persegi");
+        System.out.print("Masukkan panjang: ");
+        p = input.nextInt();
+        
+        System.out.print("Masukkan lebar: ");
+        l = input.nextInt();
+        
+        hasil = p*l;
+    
+        System.out.println("Luasnya adalah: "+hasil);
     }
-}
-
+}   
